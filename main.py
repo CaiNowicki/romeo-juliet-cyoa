dace3e96eddee227ceb2b929e6855a0b9238679b
Cai Nowicki <53185634+CaiNowicki@users.noreply.github.com>
2026-01-18 19:46:11 -0500

    Create main.py with runnable script

---
+++ main.py
@@ -0,0 +1,14 @@
+def main():
+  # this is the main function of the program
+  # everything runs from here
+    print("Romeo & Juliet: A Choose-Your-Own-Adventure")
+    print("This game will allow players to make choices that affect how the story unfolds.")
+    print("Future versions will include branching paths, tracked decisions, and multiple endings.")
+
+
+if __name__ == "__main__":
+  # this function calls the main function when the script is run
+    main()
+
+
+# also acceptable to skip function definitions, but the "main guard" is considered best practice
